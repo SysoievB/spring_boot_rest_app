96fb8aad82bdd1f45d1cd74b65691fe9823b833c
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-01-03 05:18:50 +0100

    Initial commit

---
+++ src/main/java/com/app/spring_boot_rest_app/SpringBootRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.spring_boot_rest_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestAppApplication.class, args);
+    }
+
+}
