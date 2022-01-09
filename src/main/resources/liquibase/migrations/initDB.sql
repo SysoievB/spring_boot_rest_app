3f340b0e05128bec43eda6e6b5cd07de55650c50
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-01-09 10:24:20 +0100

    added populate for db tables

--- src/main/resources/liquibase/migrations/initDB.sql
+++ src/main/resources/liquibase/migrations/initDB.sql
@@ -1,29 +1,31 @@
 --liquibase formatted sql
 --changeset sysoiev:1
 
+-- initialization of orders
 create table if not exists orders
 (
     id         bigint primary key auto_increment,
     order_name varchar(100)
 );
 
+-- initialization of accounts
 create table if not exists accounts
 (
     id     bigint primary key auto_increment,
     status enum ('ACTIVE', 'BANNED', 'DELETED') DEFAULT 'ACTIVE'
 );
 
+-- initialization of customers
 create table if not exists customers
 (
     id         bigint primary key auto_increment,
     name       varchar(155),
     surname    varchar(155),
-    order_id   bigint,
     account_id bigint,
-    foreign key (order_id) references orders (id),
     foreign key (account_id) references accounts (id)
 );
 
+-- initialization of customers_orders
 create table if not exists customers_orders
 (
     customer_id bigint not null,
--- src/main/resources/liquibase/migrations/populateDB.sql
+++ src/main/resources/liquibase/migrations/populateDB.sql
@@ -1,3 +1,25 @@
 --liquibase formatted sql
 --changeset sysoiev:2
 
+-- populate orders
+insert into orders
+values (5, 'car'),
+       (6, 'house'),
+       (7, 'plane');
+
+-- populate accounts
+insert into accounts
+values (1, 'ACTIVE'),
+       (2, 'ACTIVE'),
+       (3, 'ACTIVE');
+
+-- populate customers
+insert into customers
+values (1, 'vasia', 'vasev', 1),
+       (2, 'petia', 'sidorov', 2);
+
+-- populate customers_orders
+insert into customers_orders
+values (1, 5),
+       (1, 6),
+       (2, 7);
