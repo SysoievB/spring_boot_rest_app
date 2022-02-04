d088b55b9404c472bbdad3f6159217954dbaa6d0
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-02-04 04:47:29 +0100

    added all tests for OrderRestControllerV1Test

--- src/test/java/com/app/spring_boot_rest_app/controller/OrderRestControllerV1Test.java
+++ src/test/java/com/app/spring_boot_rest_app/controller/OrderRestControllerV1Test.java
@@ -2,6 +2,7 @@ package com.app.spring_boot_rest_app.controller;
 
 import com.app.spring_boot_rest_app.entity.Order;
 import com.app.spring_boot_rest_app.service.impl.OrderServiceImpl;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -12,10 +13,13 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 class OrderRestControllerV1Test {
@@ -34,8 +38,33 @@ class OrderRestControllerV1Test {
     @Test
     void itShouldGetAllOrders() {
         // Given
+        List<Order> orders = new ArrayList<>(Arrays.asList(
+                new Order("first"),
+                new Order("second")
+        ));
+
         // When
+        when(underTestingOrderService.list()).thenReturn(orders);
+
+        ResponseEntity<List<Order>> response = underTestingOrderRestControllerV1.getAll();
+
         // Then
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(orders.size(), Objects.requireNonNull(response.getBody()).size());
+        assertEquals(orders.get(0).getId(), response.getBody().get(0).getId());
+        assertEquals(orders.get(1).getId(), response.getBody().get(1).getId());
+    }
+
+    @Test
+    void itShouldGetAllOrdersFail() {
+        // Given
+        // When
+        when(underTestingOrderService.list()).thenReturn(new ArrayList<>());
+
+        ResponseEntity<List<Order>> response = underTestingOrderRestControllerV1.getAll();
+
+        // Then
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
@@ -72,21 +101,87 @@ class OrderRestControllerV1Test {
     @Test
     void itShouldSaveOrder() {
         // Given
+        Order order = new Order("test");
+
         // When
+        ResponseEntity<Order> response = underTestingOrderRestControllerV1.save(order);
+
         // Then
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals(order.getOrderName(), Objects.requireNonNull(response.getBody()).getOrderName());
+
+        verify(underTestingOrderService, atLeastOnce()).save(order);
+    }
+
+    @Test
+    void itShouldSaveOrderFail() {
+        // Given
+        // When
+        ResponseEntity<Order> response = underTestingOrderRestControllerV1.save(null);
+
+        // Then
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
+        verify(underTestingOrderService, never()).save(any(Order.class));
     }
 
     @Test
     void itShouldUpdateOrder() {
         // Given
+        Long orderId = 1L;
+        var order = new Order(orderId, "test");
+
         // When
+        ResponseEntity<Order> response = underTestingOrderRestControllerV1.update(orderId, order);
+
         // Then
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+        assertEquals(order.getOrderName(), Objects.requireNonNull(response.getBody()).getOrderName());
+
+        verify(underTestingOrderService, atLeastOnce()).update(orderId, order);
+    }
+
+    @Test
+    void itShouldUpdateOrderFail() {
+        // Given
+        // When
+        ResponseEntity<Order> response = underTestingOrderRestControllerV1.update(null, null);
+
+        // Then
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
+        verify(underTestingOrderService, never()).update(anyLong(), any(Order.class));
     }
 
     @Test
     void itShouldDeleteOrder() {
         // Given
+        var order = new Order(1L, "test");
+
         // When
+        when(underTestingOrderService.getById(order.getId())).thenReturn(order);
+
+        ResponseEntity<Order> response = underTestingOrderRestControllerV1.delete(order.getId());
+
         // Then
+        assertEquals(HttpStatus.ACCEPTED, response.getStatusCode());
+
+        verify(underTestingOrderService, times(1)).delete(order.getId());
+    }
+
+    @Test
+    void itShouldDeleteOrderFail() {
+        // Given
+        Long id = 1L;
+
+        // When
+        when(underTestingOrderService.getById(id)).thenReturn(null);
+
+        ResponseEntity<Order> response = underTestingOrderRestControllerV1.delete(id);
+
+        // Then
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+
+        verify(underTestingOrderService, never()).delete(anyLong());
     }
 }
