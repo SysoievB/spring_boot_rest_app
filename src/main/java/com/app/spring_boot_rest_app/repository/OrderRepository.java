07468287523a8d4789b09d456e3c28aec9e73149
SysoievB <sysoiev.bogdan.ua@gmail.com>
2022-02-06 05:51:37 +0100

    bug fixed, works!)

--- src/main/java/com/app/spring_boot_rest_app/repository/OrderRepository.java
+++ src/main/java/com/app/spring_boot_rest_app/repository/OrderRepository.java
@@ -4,10 +4,6 @@ import com.app.spring_boot_rest_app.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
-    Optional<Order> findByOrderName();
 }
